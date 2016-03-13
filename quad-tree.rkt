4d9ebfc0c7b06c99f98e47c06f71067d0007b8cb
Matias Eyzaguirre <matias@fortlogic.net>
2016-03-13 15:48:26 -0600

    Completely rewrote quad-tree.rkt.

--- quad-tree.rkt
+++ quad-tree.rkt
@@ -1,34 +1,75 @@
 #lang typed/racket/base
 
-(provide Quad-Treeof
-         (struct-out quad-leaf)
-         (struct-out quad-tree)
-         quad-tree-map
-         quad-tree-fold)
+(provide Quad-Treeof quad-tree?
+         Quad-Tree-Branch quad-tree-leaf?
+         Quad-Tree-Leaf quad-tree-branch?
+         quad-tree->s-expression
 
-(define-type (Quad-Treeof A) (U (quad-leaf A) (quad-tree A)))
+         Quadrant-Name
+         Quadrant-Path
 
-(struct (A) quad-leaf ([value : A]) #:transparent)
-(struct (A) quad-tree ([|00| : (Quad-Treeof A)]
-                       [|01| : (Quad-Treeof A)]
-                       [|10| : (Quad-Treeof A)]
-                       [|11| : (Quad-Treeof A)])
-  #:transparent)
+         qtree-ref
+         qtree-ref*
+         
+         qtree-map
+         qtree-fold)
 
-(: quad-tree-map (∀ (A B) ((A → B) (Quad-Treeof A) → (Quad-Treeof B))))
-(define (quad-tree-map f t)
-  (if (quad-tree? t)
-      (quad-tree (quad-tree-map f (quad-tree-00 t))
-                 (quad-tree-map f (quad-tree-01 t))
-                 (quad-tree-map f (quad-tree-10 t))
-                 (quad-tree-map f (quad-tree-11 t)))
+(struct (L) quad-leaf ([value : L]))
+(struct (L) quad-branch ([∨∨ : (Quad-Treeof L)]
+                         [∨∧ : (Quad-Treeof L)]
+                         [∧∨ : (Quad-Treeof L)]
+                         [∧∧ : (Quad-Treeof L)]))
+
+(define-type (Quad-Tree-Leaf L) (quad-leaf L))
+(define-type (Quad-Tree-Branch L) (quad-branch L))
+(define-type (Quad-Treeof L) (U (Quad-Tree-Leaf L)
+                                (Quad-Tree-Branch L)))
+
+(define-predicate quad-tree? (Quad-Treeof Any))
+(define-predicate quad-tree-leaf? (Quad-Tree-Leaf Any))
+(define-predicate quad-tree-branch? (Quad-Tree-Branch Any))
+
+(: quad-tree->s-expression (∀ (L) ((Quad-Treeof L) → (Sexpof L))))
+(define (quad-tree->s-expression t)
+  (if (quad-leaf? t)
+      `(leaf ,(quad-leaf-value t))
+      `(branch ,(quad-tree->s-expression (quad-branch-∨∨ t))
+               ,(quad-tree->s-expression (quad-branch-∨∧ t))
+               ,(quad-tree->s-expression (quad-branch-∧∨ t))
+               ,(quad-tree->s-expression (quad-branch-∧∧ t)))))
+
+(define-type Quadrant-Name (U '∨∨ '∨∧ '∧∨ '∧∧))
+(define-type Quadrant-Path (Listof Quadrant-Name))
+
+(: qtree-ref (∀ (L) ((quad-branch L) Quadrant-Name → (Quad-Treeof L))))
+(define (qtree-ref t q)
+  (case q
+    [(∨∨) (quad-branch-∨∨ t)]
+    [(∨∧) (quad-branch-∨∧ t)]
+    [(∧∨) (quad-branch-∧∨ t)]
+    [(∧∧) (quad-branch-∧∧ t)]))
+
+(: qtree-ref* (∀ (L) ((Quad-Treeof L) Quadrant-Path → (Quad-Treeof L))))
+(define (qtree-ref* t p)
+  (cond
+    [(null? p) t]
+    [(quad-leaf? t) (error "Cannot get children of a leaf.")]
+    [(qtree-ref* (qtree-ref t (car p)) (cdr p))]))
+
+(: qtree-map (∀ (A B) ((A → B) (Quad-Treeof A) → (Quad-Treeof B))))
+(define (qtree-map f t)
+  (if (quad-branch? t)
+      (quad-branch (qtree-map f (quad-branch-∨∨ t))
+                   (qtree-map f (quad-branch-∨∧ t))
+                   (qtree-map f (quad-branch-∧∨ t))
+                   (qtree-map f (quad-branch-∧∧ t)))
       (quad-leaf (f (quad-leaf-value t)))))
 
-(: quad-tree-fold (∀ (A B) ((B B B B → B) (A → B) (Quad-Treeof A) → B)))
-(define (quad-tree-fold node-f leaf-f t)
-  (if (quad-tree? t)
-      (node-f (quad-tree-fold node-f leaf-f (quad-tree-00 t))
-              (quad-tree-fold node-f leaf-f (quad-tree-01 t))
-              (quad-tree-fold node-f leaf-f (quad-tree-10 t))
-              (quad-tree-fold node-f leaf-f (quad-tree-11 t)))
+(: qtree-fold (∀ (A B) ((B B B B → B) (A → B) (Quad-Treeof A) → B)))
+(define (qtree-fold node-f leaf-f t)
+  (if (quad-branch? t)
+      (node-f (qtree-fold node-f leaf-f (quad-branch-∨∨ t))
+              (qtree-fold node-f leaf-f (quad-branch-∨∧ t))
+              (qtree-fold node-f leaf-f (quad-branch-∧∨ t))
+              (qtree-fold node-f leaf-f (quad-branch-∧∧ t)))
       (leaf-f (quad-leaf-value t))))
