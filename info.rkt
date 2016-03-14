b2a15f1d502d731fcdcc0b7b35f8834558bf2006
Matias Eyzaguirre <matias@fortlogic.net>
2016-03-13 21:56:20 -0600

    Updated dependencies as suggested by raco setup.

--- info.rkt
+++ info.rkt
@@ -1,7 +1,7 @@
 #lang info
 (define collection "quad-tree")
-(define deps '("base"))
-;;; (define build-deps '("scribble-lib" "racket-doc"))
+(define deps '("base" "typed-racket-lib"))
+(define build-deps '("scribble-lib" "racket-doc"))
 ;;; (define scribblings '(("scribblings/quad-tree.scrbl" ())))
 ;;; (define pkg-desc "Description Here")
 (define version "0.0.0")
