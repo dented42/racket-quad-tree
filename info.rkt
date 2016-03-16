622065610827ee7866c463958bce30cbc038b09a
Matias Eyzaguirre <matias@fortlogic.net>
2016-03-15 19:08:12 -0600

    Updated version number.

--- info.rkt
+++ info.rkt
@@ -4,5 +4,5 @@
 (define build-deps '("scribble-lib" "racket-doc"))
 ;;; (define scribblings '(("scribblings/quad-tree.scrbl" ())))
 ;;; (define pkg-desc "Description Here")
-(define version "0.1.1")
+(define version "0.1.2")
 (define pkg-authors '(dented42))
